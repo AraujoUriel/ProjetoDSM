d2f2c77175e3943574e244a1f4255c98301c82a0
URIEL MONTE PAZ DE ARAUJO <1091392413001@fatec-srv.local>
2025-09-01 20:35:25 -0300

    Definitivamente eu preciso estudar

--- scr/pages/main.js
+++ scr/pages/main.js
@@ -3,8 +3,36 @@ import { Keyboard, ActivityIndicator } from "react-native";
 import Icon from '@expo/vector-icons/MaterialIcons';
 import api from '../services/api';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { Container } from '../styles';
+import { Container, Form, Input, SubmitButton, List, User, Avatar, Name, Bio, ProfileButton, ProfileButtonText } from '../styles';
 
 export default class Main extends Component{
-    
+    state = {
+        newUser: "",
+        users: [],
+        loading: false,
+    }
+
+    componentDidMount(){
+        const users = await AsyncStorage.getItem("users");
+        if(users){
+            this.setState({users: JSON.parse(users)})
+        }
+    }
+
+    componentDidUpdate(_, prevState){
+        const {users} = this.state
+        if(prevState.uss !== users){
+            AsyncStorage.setItem("users", JSON.stringify(users))
+        }
+    }
+
+    handleAddUser = async () => {
+        try{
+            const{users, newUser} = this.state
+            this.setState({loading: true})
+            const response = await api.get(`/users/${newUser}`)
+        }catch (error){
+
+        }
+    }
 }
